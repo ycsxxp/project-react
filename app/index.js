620e408d0f2762a5c3e219a14b4298eac0b8e059
yangchao <ycsxxp@126.com>
2017-02-13 18:27:42 +0800

    update

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// import Greeter from './components/Greeter.jsx';
+import App from './components/App.jsx';
+
+ReactDom.render( < App / > ,
+	document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 //__dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录
 module.exports = { //注意这里是exports不是export
 	devtool: 'eval-source-map', //配置生成Source Maps，选择合适的选项
-	entry: __dirname + "/app/main.js", //唯一入口文件，就像Java中的main方法
+	entry: __dirname + "/app/index.js", //唯一入口文件，就像Java中的main方法
 	output: { //输出目录
 		path: __dirname + "/build", //打包后的js文件存放的地方
 		filename: "bundle.js" //打包后的js文件名
@@ -11,6 +11,21 @@ module.exports = { //注意这里是exports不是export
 			test: /\.(js|jsx)$/, //一个匹配loaders所处理的文件的拓展名的正则表达式，这里用来匹配js和jsx文件（必须）
 			exclude: /node_modules/, //屏蔽不需要处理的文件（文件夹）（可选）
 			loader: 'babel-loader' //loader的名称（必须）
+		}, {
+			test: /\.css$/, //一个匹配loaders所处理的文件的拓展名的正则表达式，这里用来匹配js和jsx文件（必须）
+			loader: 'style-loader!css-loader' //loader的名称（必须）
+		}, {
+			test: /\.scss$/, //一个匹配loaders所处理的文件的拓展名的正则表达式，这里用来匹配js和jsx文件（必须）
+			use: [{
+				loader: "style-loader" // creates style nodes from JS strings
+			}, {
+				loader: "css-loader" // translates CSS into CommonJS
+			}, {
+				loader: "sass-loader" // compiles Sass to CSS
+			}]
+		}, {
+			test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
+			loader: 'url-loader?limit=8192'
 		}]
 	},
 	devServer: {
